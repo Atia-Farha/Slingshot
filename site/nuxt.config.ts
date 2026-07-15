ff53f94d65b754a96bfcb4eb8a5b9c4077b81eef
Atia Farha <119663174+Atia-Farha@users.noreply.github.com>
2026-07-16 01:29:24 +0600

    feat(seo): add Yandex Metrika tracking scripts for enhanced analytics

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -166,7 +166,20 @@ export default defineNuxtConfig({
                     crossorigin: "",
                 },
             ],
-            script: [],
+            script: [
+                {
+                    src: "https://www.clarity.ms/tag/xmytjn7y04",
+                    async: true,
+                },
+                {
+                    innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=110772680','ym');ym(110772680,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`,
+                },
+            ],
+            noscript: [
+                {
+                    children: '<div><img src="https://mc.yandex.ru/watch/110772680" style="position:absolute; left:-9999px;" alt="" /></div>',
+                },
+            ],
         },
     },
 
